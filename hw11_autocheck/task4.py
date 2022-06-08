767b7f084b87b49530b4de15c3484bc827145906
shuaaam <oleksandramelnyk.ibm@gmail.com>
2022-06-08 15:14:13 +0300

    11/4

--- hw11_autocheck/task4.py
+++ hw11_autocheck/task4.py
@@ -8,3 +8,44 @@
 Для записи значения координат вектора через индекс, как vector[0] = 10, реализуйте метод __setitem__ у класса Vector.
 Обращение к координате x производится по индексу 0, а обращение к координате y — по индексу 1.'''
 
+class Point:
+    def __init__(self, x, y):
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if (type(x) == int) or (type(x) == float):
+            self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if (type(y) == int) or (type(y) == float):
+            self.__y = y
+
+
+class Vector:
+    def __init__(self, coordinates: Point):
+        self.coordinates = coordinates
+
+    def __setitem__(self, index, value):
+        if index == 0:
+            self.coordinates.x = value
+        elif index == 1:
+            self.coordinates.y = value
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.coordinates.x
+        elif index == 1:
+            return self.coordinates.y
